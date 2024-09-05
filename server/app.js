8c70a51a9b599b60c89216b1ef80dc40c74ccade
YahyaNashar22 <yahyanashar22@gmail.com>
2024-09-05 12:58:59 +0300

    started the server and initialized db connection

---
+++ server/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from 'body-parser';
+
+import dbConnection from './db/dbConnection.js';
+
+dotenv.config();
+const app = express();
+
+//CORS Policies
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
+));
+
+//essential middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(process.env.PORT, (error) => {
+    if (!error) {
+        console.log(`Server running on port: ${process.env.PORT}`);
+    } else {
+        console.log(`Error: ${error}`);
+    }
+});
+dbConnection();
